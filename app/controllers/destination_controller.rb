242b8ae7bf739df1677ffa164219aaa9bd745169
Noah <crashlive112@gmail.com>
2022-05-26 21:28:35 -0400

    include notes as nested json

--- app/controllers/destination_controller.rb
+++ app/controllers/destination_controller.rb
@@ -1,11 +1,11 @@
 class DestinationsController < ApplicationController
   get "/destinations" do
     destinations = Destination.all
-    destinations.to_json
+    destinations.to_json(include: :notes)
   end
 
   get "/destinations/:id" do
     destination = Destination.find(params[:id])
-    destination.to_json
+    destination.to_json(include: :notes)
   end
 end
